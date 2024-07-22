740da2defb58ba036fe82e99106734b3488fa6ec
Enrico Deiana <enrico.deiana.design@gmail.com>
2024-07-23 00:44:37 +0200

    Update homemade-effects.js

--- homemade-effects.js
+++ homemade-effects.js
@@ -1,52 +1,52 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const links = document.querySelectorAll('a');
-  const isHomePage = window.location.pathname === '/' || window.location.pathname === '/index.html';
+// Code that runs on click of a link
+$(window).on('load', function() {
+	// AVVIO
 
-  if (!isHomePage) {
-    // Code that runs on pageload for other pages
-    gsap.set(".load_grid", { display: "grid" });
-    gsap.to(".load_grid-item", {
-      opacity: 0,
-      duration: 0.3,
-      stagger: { amount: 0.5, from: "random" },
-      onComplete: () => {
-        gsap.set(".load_grid", { display: "none" });
-      }
-    });
-  }
 
-  links.forEach(link => {
-    link.addEventListener('click', function (e) {
-      if (
-        this.hostname === window.location.hostname &&
-        !this.href.includes('#') &&
-        this.target !== '_blank'
-      ) {
+  // Code that runs on pageload
+
+  gsap.to(".load_grid-item", {
+    opacity: 0,
+    duration: 0.3,
+    stagger: { amount: 0.5, from: "random" },
+    onComplete: () => {
+      gsap.set(".load_grid", { display: "none" });
+    }
+  });
+
+	
+
+  $("a").on("click", function (e) {
+    if (
+    	$(this).prop("hostname") === window.location.host &&
+      $(this).attr("href").indexOf("#") === -1 &&
+      $(this).attr("target") !== "_blank") {
         e.preventDefault();
-        let destination = this.href;
-        gsap.set('.load_grid', { display: 'grid' });
+        let destination = $(this).attr("href");
+        gsap.set(".load_grid", { display: "grid" });
         gsap.fromTo(
-          '.load_grid-item',
-          { opacity: 0 },
+          ".load_grid-item",
+          {
+            opacity: 0
+          },
           {
             opacity: 1,
             duration: 0.001,
-            stagger: { amount: 0.5, from: 'random' },
+            stagger: { amount: 0.5, from: "random" }, //you can also try a from: "start" or "end" -- get creative!
             onComplete: () => {
               window.location = destination;
             }
           }
         );
-      }
-    });
-  });
-
-  // Handle back button navigation
-  window.onpageshow = function (event) {
-    if (event.persisted) {
-      window.location.reload();
     }
-  };
+  });
+  
+  // On click of the back button
+  window.onpageshow = function(event){
+  	if (event.persisted) {
+    	window.location.reload();
+    }
+  }
 });
 
 
